72724bd57401b92d480dd0dac98f59898acb14a0
Ashfaq147 <124809666+Ashfaq147@users.noreply.github.com>
2023-05-18 01:04:54 +0530

    Add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Box, Grid, IconButton, Typography } from '@mui/material';
+import { ArrowBackIos, ArrowForwardIos, PlayArrow, Pause } from '@mui/icons-material';
+import { images } from './images';
 
-function App() {
+const CatalogViewer = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isSlideshowRunning, setIsSlideshowRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | null = null;
+
+    if (isSlideshowRunning) {
+      intervalId = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+      }, 3000);
+    }
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [isSlideshowRunning]);
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handleThumbnailClick = (index: number) => {
+    setCurrentIndex(index);
+    setIsSlideshowRunning(false);
+  };
+
+  const handlePlayPause = () => {
+    setIsSlideshowRunning((prevIsRunning) => !prevIsRunning);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <img src={images[currentIndex].url} alt={images[currentIndex].title} style={{ maxWidth: '100%' }} />
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <IconButton onClick={handlePrevious}>
+              <ArrowBackIos />
+            </IconButton>
+            <IconButton onClick={handlePlayPause}>
+              {isSlideshowRunning ? <Pause /> : <PlayArrow />}
+            </IconButton>
+            <IconButton onClick={handleNext}>
+              <ArrowForwardIos />
+            </IconButton>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box>
+            <Typography variant="h5" gutterBottom>
+              {images[currentIndex].title}
+            </Typography>
+            <Typography variant="body1">{images[currentIndex].description}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.title}
+                style={{
+                  width: '80px',
+                  height: '80px',
+                  margin: '0 4px',
+                  filter: index === currentIndex ? 'none' : 'grayscale(100%)',
+                  cursor: 'pointer',
+                }}
+                onClick={() => handleThumbnailClick(index)}
+              />
+            ))}
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
-}
+};
 
-export default App;
+export default CatalogViewer;
---
+++ src/images.tsx
@@ -0,0 +1,48 @@
+export const images = [
+  {
+    id: 1,
+    url: 'https://cdn.pixabay.com/photo/2014/06/27/12/36/fish-378286_960_720.jpg',
+    header: "UNDERWATER FISH",
+    description: "An enchanting underwater scene featuring a school of colorful fish swimming amidst vibrant coral reefs. The image showcases the beauty and diversity of marine life in a tranquil aquatic environment.",
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    id: 2,
+    url: 'https://cdn.pixabay.com/photo/2013/02/01/18/14/url-77169_960_720.jpg',
+    header: "BOATS",
+    description: "A picturesque view of boats docked at a serene harbor. The image captures the essence of maritime life, with colorful boats gently swaying in calm waters against a backdrop of clear blue sky and distant mountains.",
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    id: 3,
+    url: 'https://cdn.pixabay.com/photo/2016/03/08/20/03/flag-1244649_960_720.jpg',
+    header: "FLOWERS",
+    description: "A vibrant collection of blooming flowers showcasing a delightful array of colors and textures. The image highlights nature's beauty, featuring various species of flowers arranged harmoniously to create a visually appealing composition.",
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    url: 'https://media.istockphoto.com/id/1369510216/photo/carefree-beautiful-latin-woman-relaxing-on-beach.jpg?s=1024x1024&w=is&k=20&c=MAZMFd7Xi490fU05LTjj7EyuGuvNefJUZ-OgLDLXSMM=',
+    alt: 'Image 1',
+    title: 'Beautiful Latin Woman',
+    description: 'A carefree beautiful Latin woman relaxing on the beach with closed eyes at sunset. She is enjoying the wind fluttering her hair. This charming and calm young woman is breathing in the fresh air at the summer beach. The image also has copy space.',
+  },
+  {
+    url: 'https://media.istockphoto.com/id/1402004971/photo/healthy-heart-food-high-in-flavonoids-and-polyphenols.jpg?s=1024x1024&w=is&k=20&c=tNHyUcoK9Veqhs5mfNenLTyQ6PTjm-WR4SL_WZEAgQ4=',
+    alt: 'Image 2',
+    title: 'Healthy Heart Food',
+    description: 'This image showcases a variety of healthy heart food items that are rich in flavonoids, polyphenols, antioxidants, anthocyanins, lycopene, vitamins, proteins, bioflavonoids, minerals, and fiber. The food items are presented on a rustic wood background.',
+  },
+  {
+    url: 'https://media.istockphoto.com/id/1347086252/photo/common-chimpanzee.jpg?s=1024x1024&w=is&k=20&c=mZw8Xizct1h4nxzmEYwkEHmRloUcDuHLohOirvkJ9YI=',
+    alt: 'Image 3',
+    title: 'Common Chimpanzee',
+    description: 'This portrait image features a common chimpanzee (Pan troglodytes), a species of great ape native to the forests and savannahs of Africa. The chimpanzee is known for its intelligence and close genetic relation to humans.',
+  },
+  {
+    url: 'https://media.istockphoto.com/id/1417433518/photo/pine-tree-forest-on-a-hill-desktop-background.jpg?s=1024x1024&w=is&k=20&c=0RIgbgqrlXqJw6Kw_zRJaQN2BtCfwSVfGL0uyHrIBk0=',
+    alt: 'Image 4',
+    title: 'Pine Tree Forest on a Hill',
+    description: 'This picturesque image showcases a scenic view of a pine tree forest located on a hill. The image can be used as a desktop background or wallpaper to create a serene and calming atmosphere.',
+  },
+  // Add more images here if needed
+];
